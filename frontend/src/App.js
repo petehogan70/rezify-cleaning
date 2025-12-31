676513cf1a7aab8b53abc0a77e68c516db67e48f
Peter Hogan <112176057+petehogan70@users.noreply.github.com>
2025-12-30 23:50:53 -0600

    Deleted all uneccesary files for cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,6 @@
-import { NotFound } from './objects/NotFound';
 import { Index } from './objects/Index';
-import { Login } from './objects/Login';
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import './styles/NewIndexPage.css'
-import { Register } from './objects/Register';
-import { Results } from './objects/Results';
-import { Recover } from './objects/Recover';
-import { PostJob } from './objects/PostJob';
-import { GetDemo } from './objects/GetDemo';
-import {Feedback} from './objects/Feedback';
-import {Help} from './objects/Help';
-import './styles/DefaultPage.css'
-import { updateColors, useTheme } from './hooks/ThemeContext';
-import { Plans } from './objects/Plans';
-import { Payment } from './objects/Payment';
-import { Profile } from './objects/Profile';
-import { AdminDashboard } from './objects/AdminDashboard';
 
 function App() {
   return (
@@ -30,71 +14,13 @@ function App() {
 }
 
 function AppDetails() {
-  const {setTheme} = useTheme();
-  const [firstTimer, setFirstTimer] = useState(true); //indicates no theme decided, don't render colors yet...
-  useEffect(() => {
-      const savedThemeData = localStorage.getItem('theme-colors'); //check if website has theme saved already
-      if (savedThemeData) { //if theme exists, apply those colors
-        const savedColors = JSON.parse(savedThemeData)
-        updateColors(savedColors, setTheme);
-        setFirstTimer(false); //user has data already
-      }
-  }, [])
 
   const location = useLocation();
-  useEffect(() => {
-    document.body.className = ''; //clear class css specification
-    if (location.pathname === '/') {
-      document.body.classList.add('index');
-    } else if (location.pathname === '/login') {
-      document.body.classList.add('login');
-    } else if (location.pathname === '/register' || location.pathname === '/verify_email' || location.pathname === '/recover' || location.pathname === '/change_password') {
-      document.body.classList.add('register');
-    } else if (location.pathname === '/results') {
-      document.body.classList.add('results');
-    } else if (location.pathname === '/post_job') {
-      document.body.classList.add('postjob');
-    } else if (location.pathname === '/get_demo') {
-      document.body.classList.add('getdemo');
-    } else if (location.pathname === '/plans') {
-      document.body.classList.add('plans');
-    } else if (location.pathname === '/payment') {
-      document.body.classList.add('payment');
-    } else if (location.pathname === '/profile') {
-      document.body.classList.add('profile');
-    } else if (location.pathname === '/admin') {
-      document.body.classList.add('admin');
-      } else if (location.pathname === '/feedback') {
-      document.body.classList.add('feedback');
-    } else if (location.pathname === '/help') {
-      document.body.classList.add('help');
-    } else {
-      document.body.classList.add('index'); //not found page
-    }
-  }, [location]);
 
   return (
     <>
         <Routes>
-          <Route path="/" element={<Index firstTimer={firstTimer}/>} />
-          <Route path="/index" element={<Index  firstTimer={firstTimer}/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/verify_email" element={<Register verify={true}/>} />
-          <Route path="/results" element={<Results/>} />
-          <Route path="/recover" element={<Recover recover={true}/>} />
-          <Route path="/change_password" element={<Recover recover={false}/>} />
-          <Route path="/post_job" element={<PostJob/>} />
-          <Route path="/get_demo" element={<GetDemo/>} />
-          <Route path="/:school" element={<Index firstTimer={firstTimer}/>} />
-          <Route path="/plans" element={<Plans/>}/>
-          <Route path="/payment" element={<Payment/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/admin" element={<AdminDashboard/>} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/help" element={<Help />} />
-          <Route path="*" element={<NotFound/>} />
-
+          <Route path="/" element={<Index/>} />
         </Routes>      
     </>
   );
--- frontend/src/objects/Index.js
+++ frontend/src/objects/Index.js
@@ -1,71 +1,19 @@
-import { IndexBody } from './IndexBody';
-import { IndexFooter } from './IndexFooter';
-import '../styles/NewIndexPage.css';
-import { useState, useEffect } from 'react';
-import { useSearchParams, useParams, useNavigate } from 'react-router-dom';
-import { updateColors, useTheme } from '../hooks/ThemeContext';
-
-
-function Index({firstTimer = false}) {
-    const [currUser, setCurrentUser] = useState({});
-    const [currErrMsg, setCurrErrMsg] = useState("");
-    const [isLoaded, setIsLoaded] = useState(false); //backend response complete or not
-    const {setTheme} = useTheme();
-    const [searchParams] = useSearchParams();
-    
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        fetch(`/api`).then(async response => {
-          //get flask backend response
-          if (response.ok) {
-            const data = await response.json();
-            if (data.colors) {
-                localStorage.setItem('theme-colors', JSON.stringify(data.colors));
-                updateColors(data.colors, setTheme)
-            }
-            
-            if (data.should_redirect  && searchParams.get("noredir") !== "1") {
-              navigate("/results");
-            }
-
-            if (data.redirect_admin) {
-                navigate("/admin");
-            }
-
-            //get user if exists
-            if (data.user) {
-                //const userJson = await data.user.json();
-                setCurrentUser(data.user);
-            } else {
-                setCurrentUser({
-                    'na': true
-                })
-            }
-
-            if (data.error_message) {
-                setCurrErrMsg(data.error_message)
-            }
-            setIsLoaded(true);
-          } else {
-            setCurrentUser({'error': true})
-            setCurrErrMsg("Error: " + response.statusText)
-            //error, prob shouldnt log in
-            setIsLoaded(true);
-          }
-        })
-      }, []);
-    return (<>
-        {(firstTimer && !isLoaded) ?
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',  height: '100vh', width: '100vw'}}>
-          <div className='spinner'></div>
-        </div>
-        :
-        <>
-          <IndexBody user={currUser} errormessage={currErrMsg}/>
-          <IndexFooter/>
-        </>}
-        </>);
+function Index() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '2rem',
+        fontWeight: '600'
+      }}
+    >
+      Hello World
+    </div>
+  );
 }
 
-export {Index};
+export { Index };
